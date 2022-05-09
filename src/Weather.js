854f541f78bb5ede0943a3fda35d772b0eea3940
John Marcos <johnsaguay@gmail.com>
2022-05-09 09:56:41 -0700

    added to Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,29 @@
 import { useState } from 'react';
+import './Weather.css';
 
 function Weather() {
+    const [zip, setZip] = useState('94210');
+    const [unit, setUnit] = useState('');
     return (
-        <div>
+        <div className='Weather'>
+            <h1>{zip}</h1>
+            <h1>{unit}</h1>
             <form>
-                <></>
+                <div>
+                    <input
+                        placeholder='Enter a Zip Code'
+                        value={zip}
+                        onChange={(e) => setZip(e.target.value)} />
+                    <button>Submit</button>
+                </div>
+                
+                <select onChange={ e => setUnit(e.target.value)}>
+                    <option value="metric">Celcius</option>
+                    <option value="imperial">Fahrenheit</option>
+                    <option value="standard">Kelvin</option>
+                </select>
             </form>
         </div>
     )
 }
+export default Weather;
